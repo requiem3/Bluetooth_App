c48156adfc37f79aa1af296d14177f1c0e57ac5a
Kyle Heitman <kyle.heitman@uconn.edu>
2015-02-09 16:44:29 -0500

    added chat, constants, and threads classes
    Chat: Class for managing the chat, connecting, and listening that will
    be used in the app.
    Threads: Class that will contain all the multithreading code used in the
    Chat class.
    Constants: class for holding all the constant values, makes it easier to
    find shit and what not.

---
+++ Bluetooth_App/src/com/example/bluetooth_app/Constants.java
@@ -0,0 +1,12 @@
+package com.example.bluetooth_app;
+
+public class Constants {
+	public int VISIBLE = 0;
+	public int INVISIBLE = 4;
+	
+	public int SNONE = 0;
+	public int SLISTEN = 1;
+	public int SCONNECTED = 2;
+	public int SCONNECTING = 3;
+	
+}
